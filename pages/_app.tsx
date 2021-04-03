76ea32045eb238cc2663c1991fa3df110e51037e
mouhsine bakhich <mouhsine.bakhich@gmail.com>
2021-04-03 15:51:54 +0400

    add react query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
 import 'tailwindcss/tailwind.css'
+import { useRef } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { Hydrate } from 'react-query/hydration'
+
+// Create a client
+const queryClient = new QueryClient()
 
 // https://pokeapi.co/api/v2/pokemon?offset=20&limit=20
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClientRef = useRef<QueryClient>()
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient()
+  }
+
+  return <QueryClientProvider client={queryClient}>
+    <Hydrate state={pageProps.dehydratedState}>
+      <Component {...pageProps} />
+    </Hydrate>
+  </QueryClientProvider>
 }
 
 export default MyApp
